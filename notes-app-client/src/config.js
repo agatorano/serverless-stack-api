b631b888be512993ae174b0e2fed9d2d6fa8e02f
agatorano <agatorano@gmail.com>
2020-01-11 13:08:56 -0600

    Setting up our React app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-upload-3"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://iydcdu87xk.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_i9mGMJnV6"
+    APP_CLIENT_ID: "1emo4k8p868jfckttf03ds0fos",
+    IDENTITY_POOL_ID: "us-east-2:ffb31aa5-3854-43a3-88ef-1b7a1214dc67"
+  }
+};
